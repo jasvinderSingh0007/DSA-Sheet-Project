4927fca0702dae08ce781c831aa54c5d5e99068f
jasvinderSingh0007 <147312125+jasvinderSingh0007@users.noreply.github.com>
2025-06-12 20:00:33 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'https://dsa-sheet-project-frontend.onrender.com', credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
